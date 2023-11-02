78e367de35a4635e018b98fccce165eab276e515
Luis Diego Ruiz Bautista <luis290613@gmail.com>
2023-11-02 15:55:41 -0500

    Initial Commit

---
+++ OnboardingCarouselView/OnboardingCarouselViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingCarouselViewApp.swift
+//  OnboardingCarouselView
+//
+//  Created by Diego on 2/11/23.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingCarouselViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
